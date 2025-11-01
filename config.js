ce4e4c383d2ffa76e14ac3ceb2604cb2b492a7b9
THARUWA <111736808+Tharusha727@users.noreply.github.com>
2025-11-01 19:11:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~9ZwlWQhQ#EmWWGUPM_DZtFEj5611rux9ESpnQutdC80TyPPdWSks'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?XmSh8p'
 };
